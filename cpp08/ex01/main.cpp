003f25f3803569ec3f4aac2bebefac4a30980eac
moi <alexdiehlboitel@gmail.com>
2026-06-01 04:51:56 +0200

    workge

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -10,5 +10,30 @@ int main()
     sp.addNumber(11);
     std::cout << sp.shortestSpan() << std::endl;
     std::cout << sp.longestSpan() << std::endl;
+    try {
+        sp.addNumber(99);
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl; // Span is full
+    }
+
+    // test when not enough numbers
+    try {
+        Span sp2(5);
+        sp2.addNumber(1);
+        sp2.shortestSpan();
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl; // Not enough numbers
+    }
+
+    // test addRange with a big range
+    Span sp3(10000);
+    std::vector<int> bigVec;
+    for (int i = 0; i < 10000; i++)
+        bigVec.push_back(i);
+
+    sp3.addRange(bigVec.begin(), bigVec.end());
+    std::cout << sp3.shortestSpan() << std::endl; // 1
+    std::cout << sp3.longestSpan() << std::endl;  // 9999
+
     return 0;
 }
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,5 +1,7 @@
 #include "MutantStack.hpp"
 
+#include <iostream>
+
 int main()
 {
     MutantStack<int> mstack;
@@ -11,7 +13,6 @@ int main()
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
@@ -23,5 +24,29 @@ int main()
         ++it;
     }
     std::stack<int> s(mstack);
+    
+    // empty check
+    MutantStack<int> empty;
+    if (empty.empty())
+        std::cout << "empty: yes" << std::endl;
+    else
+        std::cout << "empty: no" << std::endl;
+    // copy constructor
+    MutantStack<int> copy(mstack);
+    std::cout << "copy size: " << copy.size() << std::endl; // same as mstack
+
+    // assignment operator
+    MutantStack<int> assigned;
+    assigned = mstack;
+    std::cout << "assigned top: " << assigned.top() << std::endl; // 0
+
+    // iterate over copy to confirm same content
+    MutantStack<int>::iterator it2 = copy.begin();
+    while (it2 != copy.end())
+    {
+        std::cout << *it2 << std::endl;
+        ++it2;
+    }
+
     return 0;
 }
